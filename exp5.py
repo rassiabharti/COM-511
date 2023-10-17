57a71ae5adb6fea9dcc9938a268f8a48294db7df
rassiabharti <113895546+rassiabharti@users.noreply.github.com>
2023-10-17 21:46:17 +0530

    Create exp5.py

---
+++ exp5.py
@@ -0,0 +1,20 @@
+# #normal
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [10, 11, 12]
+]
+
+print("Original matrix")
+for row in matrix:
+    print(row)
+    
+k = int(input("Enter the row you want to reverse: "))
+
+for i in range(k - 1, len(matrix), k):
+    matrix[i] = matrix[i][::-1]
+
+print("After reversing every", k, "th row, the matrix is:")
+for row in matrix:
+    print(row)
